8a7ac86a490662952fc1d634924c9c17a3b45e9c
nwendel16 <54493713+nwendel16@users.noreply.github.com>
2019-10-21 15:51:30 -0700

    Add files via upload

---
+++ Module 1/python_basics.py
@@ -0,0 +1,43 @@
+#Nick Wendel
+#IT - 75
+#Python Basics assignment
+
+
+#1
+
+print('#1')
+print("\n")
+sbsp = 'SpongeBob SquarePants'
+print(sbsp)
+sbsp = 'Lives in a pineapple under the sea'
+print(sbsp)
+print("\n")
+
+#2
+
+print('#2')
+print("\n")
+tm = '"Life is a fight. Do not let it overwhelm you. Adapt, and combat every situation it throws at you."'
+print(tm + ' -Tim McIlrath')
+print("\n")
+
+#3
+
+print('#3')
+print("\n")
+fav = ('My favorite # is')
+print('My favorite # is' + '... ' + str(len(fav)))
+print("\n")
+
+#4
+
+print('#4')
+print("\n")
+rv = """Once upon a midnight dreary, while I pondered, weak and weary,
+Over many a quaint and curious volume of forgotten lore,
+While I nodded, nearly napping, suddenly there came a tapping,
+As of some one gently rapping, rapping at my chamber door.
+'Tis some visitor, I muttered, tapping at my chamber door;
+Only this and nothing more."""
+num_chars = (len(str(rv)))
+print(num_chars)
